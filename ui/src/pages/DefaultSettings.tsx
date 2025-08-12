44b7191afbbe1d8d249811e7054f515562607be1
Carlyyb <3022402752@qq.com>
2025-08-12 23:34:16 +0800

    test ui

--- ui/src/pages/DefaultSettings.tsx
+++ ui/src/pages/DefaultSettings.tsx
@@ -7,7 +7,7 @@ import { getSettings } from '../graphql/queries';
 import { upsertSettings } from '../graphql/mutations';
 import { optionise } from '../helpers';
 import { DispatchAlertContext, AlertType } from '../contexts/alerts';
-import { setCurrentLang } from '../i18n/lang';
+import { setCurrentLang, getText } from '../i18n/lang';
 
 const client = generateClient();
 
@@ -46,25 +46,25 @@ export default () => {
             query: upsertSettings,
             variables: { input: { uiLang: uiLang?.value as Lang, docLang: docLang?.value as Lang, assessType: assessType?.value as AssessType } },
           })
-          .then(() => dispatchAlert({ type: AlertType.SUCCESS, content: 'Settings updated successfully' }))
-          .catch(() => dispatchAlert({ type: AlertType.ERROR }));
+          .then(() => dispatchAlert({ type: AlertType.SUCCESS, content: getText('pages.settings.update_success') }))
+          .catch(() => dispatchAlert({ type: AlertType.ERROR, content: getText('common.error') }));
       }}
     >
       <Form
         actions={
           <SpaceBetween direction="horizontal" size="xs">
             <Button formAction="none" variant="link">
-              Cancel
+              {getText('common.cancel')}
             </Button>
-            <Button variant="primary">Submit</Button>
+            <Button variant="primary">{getText('common.submit')}</Button>
           </SpaceBetween>
         }
-        header={<Header variant="h1">Default Settings</Header>}
+        header={<Header variant="h1">{getText('pages.settings.title')}</Header>}
       >
         <Container>
           <Box padding="xxxl">
             <SpaceBetween direction="horizontal" size="l">
-              <FormField label="UI Language">
+              <FormField label={getText('pages.settings.ui_language')}>
                 <Select 
                   options={langs} 
                   selectedOption={uiLang} 
@@ -77,13 +77,13 @@ export default () => {
                   }} 
                 />
               </FormField>
-              <FormField label="Document Language">
+              <FormField label={getText('pages.settings.doc_language')}>
                 <Select options={langs} selectedOption={docLang} onChange={({ detail }) => setDocLang(detail.selectedOption)} />
               </FormField>
-              <FormField label="Default Assessment Type">
+              <FormField label={getText('pages.settings.default_assessment_type')}>
                 <Select options={assessTypes} selectedOption={assessType} onChange={({ detail }) => setAssessType(detail.selectedOption)} />
               </FormField>
-              <FormField label="Default Taxonomy">
+              <FormField label={getText('pages.settings.default_taxonomy')}>
                 <Select options={taxonomies} selectedOption={taxonomy} onChange={({ detail }) => setTaxonomy(detail.selectedOption)} />
               </FormField>
             </SpaceBetween>
